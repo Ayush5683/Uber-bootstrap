0e8ed4a9c5577eff2134911a67c138b137cb5114
Ayush5683 <141903522+Ayush5683@users.noreply.github.com>
2024-12-18 21:48:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showNav(){
+    if(document.getElementById("nestedNav").style.display==="block"){
+        document.getElementById("nestedNav").style.display="none";
+    }
+    else{
+        document.getElementById("nestedNav").style.display="block";
+    }
+}
