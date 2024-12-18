32696764565d227ac5393cd76b8a8606aecec0b2
Ayush5683 <141903522+Ayush5683@users.noreply.github.com>
2024-12-19 00:14:51 +0530

    Add files via upload

---
+++ fleet.js
@@ -0,0 +1,18 @@
+function showNav(){
+    if(document.getElementById("nestedNav").style.display==="block"){
+        document.getElementById("nestedNav").style.display="none";
+    }
+    else{
+        document.getElementById("nestedNav").style.display="block";
+    }
+}
+
+function hideShow(){
+    if(document.querySelector('.hide').style.display==="none")
+    {
+        document.querySelector('.hide').style.display="block";
+    }
+    else{
+        document.querySelector('.hide').style.display="none";
+    }
+}
