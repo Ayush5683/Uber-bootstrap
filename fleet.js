3f663dc302db17a158a8790dc13f495b41df1bd5
Ayush5683 <141903522+Ayush5683@users.noreply.github.com>
2024-12-20 00:28:18 +0530

    Add files via upload

--- fleet.js
+++ fleet.js
@@ -7,12 +7,48 @@ function showNav(){
     }
 }
 
-function hideShow(){
-    if(document.querySelector('.hide').style.display==="none")
+function hideShow1(){
+    if(document.querySelector('.hide1').style.display==="none")
     {
-        document.querySelector('.hide').style.display="block";
+        document.querySelector('.hide1').style.display="block";
     }
     else{
-        document.querySelector('.hide').style.display="none";
+        document.querySelector('.hide1').style.display="none";
+    }
+}
+function hideShow2(){
+    if(document.querySelector('.hide2').style.display==="none")
+    {
+        document.querySelector('.hide2').style.display="block";
+    }
+    else{
+        document.querySelector('.hide2').style.display="none";
+    }
+}
+function hideShow3(){
+    if(document.querySelector('.hide3').style.display==="none")
+    {
+        document.querySelector('.hide3').style.display="block";
+    }
+    else{
+        document.querySelector('.hide3').style.display="none";
+    }
+}
+function hideShow4(){
+    if(document.querySelector('.hide4').style.display==="none")
+    {
+        document.querySelector('.hide4').style.display="block";
+    }
+    else{
+        document.querySelector('.hide4').style.display="none";
+    }
+}
+function hideShow5(){
+    if(document.querySelector('.hide5').style.display==="none")
+    {
+        document.querySelector('.hide5').style.display="block";
+    }
+    else{
+        document.querySelector('.hide5').style.display="none";
     }
 }
